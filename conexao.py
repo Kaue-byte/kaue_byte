729e467aa0d3bbc4e2a1b6240f3ece6bba0f89b3
unknown <mobsters430@gmail.com>
2025-02-18 18:39:37 -0300

    Alterações PARA O BD

--- conexao.py
+++ conexao.py
@@ -1,15 +1,21 @@
 import mysql.connector
 
-
 def conectar():
     try:
         mydb = mysql.connector.connect(
-            host="localhost", 
-            user="root",
-            password="",
-            database="repre"
+            host="metro.proxy.rlwy.net",  # Host do Railway
+            user="root",  # Usuário do Railway
+            password="NWqLfnRFkeQDLvIVsoLYnSXrjFYnPtLq",  # Senha do Railway
+            database="railway",  # Nome do banco de dados
+            port=19986  # Porta do Railway
         )
+        print("Conexão bem-sucedida!")
         return mydb
     except mysql.connector.Error as err:
         print(f"Erro ao conectar ao banco de dados: {err}")
         return None
+
+# Testando a conexão
+conexao = conectar()
+if conexao:
+    conexao.close()
